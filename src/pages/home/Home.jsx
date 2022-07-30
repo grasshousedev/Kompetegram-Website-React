d07e0a48e27e6546b0f047f4c53b0ed585f7184f
Oleksandr F <boltbright525@gmail.com>
2022-07-31 05:06:38 +0700

    header and section 1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,8 @@ import './styles/Mobile.css';
 
 import ktgLogo from '../../assets/img/logo.svg';
 import ktgLogoTxt from '../../assets/img/Kompetegram.svg'
+import arrowRedirect from '../../assets/img/Arrow-Redirect.svg'
+import orbit1 from '../../assets/img/Illustration-Orbit.svg'
 
 class Home extends Component {
 
@@ -24,10 +26,32 @@ class Home extends Component {
         return (
 
             <div className="Home">
-                <div className="header">
-                    <img src={ ktgLogo } alt="" id='logo'/>
-                    <img src={ ktgLogoTxt } alt="" id='logo-text'/>
+                <div className="Header">
+                    <img src={ ktgLogo } alt="" id="logo"/>
+                    <img src={ ktgLogoTxt } alt="" id="logo-text"/>
                 </div>
+
+                <div className="Section-1">
+                    <div className="Header">
+                        <h1 id="header-txt">Komunitas, Teknologi, dan Pemrograman</h1>
+                    </div>
+
+                    <div className="Description">
+                        <p id="description-txt">Untuk menjadi programmer yang baik salah satunya membutuhkan komunitas. Kami menyediakan kerjasama, skill upgrading, dan ide-ide untuk solusi</p>
+                    </div>
+                    
+                    <div className="Register-Button">
+                        <a href="/" id="regis-btn">
+                            <img src={ arrowRedirect } alt="" id="arrow-logo"/>
+                            Daftar
+                        </a>
+                    </div>
+
+                    <div className="Illustration">
+                        <img src={ orbit1 } alt="" id='illustration-img'/>
+                    </div>
+                </div>
+                
             </div>
 
         );
