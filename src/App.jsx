5765d5064ea128a70539bceebc4b59a739e37512
Oleksandr F <boltbright525@gmail.com>
2022-07-30 22:55:28 +0700

    First

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/home/Home';
+
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+    
+    this.state = {
+
+    }
+
+    return;
+  }
+
+  render() {
+
+    return (
+
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route exact path='/' element={<Navigate to='/home' />} />
+            <Route exact path='/home' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import './styles/Global.css'
+
+class Home extends Component {
+
+    constructor(props) {
+        
+        super(props);
+
+        this.state = {
+
+        }
+
+        return;
+    }
+
+    render() { 
+        return (
+            <div className="Home">
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Home;
