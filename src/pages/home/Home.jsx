d278115b29ae22016de3b754f4cd3026e7e2c561
Oleksandr F <boltbright525@gmail.com>
2022-07-31 18:39:23 +0700

    section 1 and 2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,12 +6,13 @@ import './styles/Mobile.css';
 import ktgLogo from '../../assets/img/logo.svg';
 import ktgLogoTxt from '../../assets/img/Kompetegram.svg'
 import arrowRedirect from '../../assets/img/Arrow-Redirect.svg'
-import orbit1 from '../../assets/img/Illustration-Orbit.svg'
+import illustration1 from '../../assets/img/Illustration-Orbit.svg'
+import illustration2 from '../../assets/img/Illustration-Orbit-Community.svg'
 
 class Home extends Component {
 
     constructor(props) {
-        
+
         super(props);
 
         this.state = {
@@ -21,14 +22,14 @@ class Home extends Component {
         return;
     }
 
-    render() { 
+    render() {
 
         return (
 
             <div className="Home">
                 <div className="Header">
-                    <img src={ ktgLogo } alt="" id="logo"/>
-                    <img src={ ktgLogoTxt } alt="" id="logo-text"/>
+                    <img src={ktgLogo} alt="" id="logo" />
+                    <img src={ktgLogoTxt} alt="" id="logo-text" />
                 </div>
 
                 <div className="Section-1">
@@ -37,25 +38,55 @@ class Home extends Component {
                     </div>
 
                     <div className="Description">
-                        <p id="description-txt">Untuk menjadi programmer yang baik salah satunya membutuhkan komunitas. Kami menyediakan kerjasama, skill upgrading, dan ide-ide untuk solusi</p>
+                        <p id="desc-txt">Untuk menjadi programmer yang baik salah satunya membutuhkan komunitas. 
+                        Kami menyediakan kerjasama, skill upgrading, dan ide-ide untuk solusi.
+                        </p>
                     </div>
-                    
+
                     <div className="Register-Button">
                         <a href="/" id="regis-btn">
-                            <img src={ arrowRedirect } alt="" id="arrow-logo"/>
+                            <img src={arrowRedirect} alt="" id="arrow-logo" />
                             Daftar
                         </a>
                     </div>
 
                     <div className="Illustration">
-                        <img src={ orbit1 } alt="" id='illustration-img'/>
+                        <img src={illustration1} alt="" id='illus-img' />
                     </div>
                 </div>
-                
+
+                <div className="Section-2">
+                    <div className="Illustration">
+                        <img src={illustration2} alt="" id='illus-img' />
+                    </div>
+
+                    <div className="Header">
+                        <h1 id='header-txt'>Tentang Kami</h1>
+                    </div>
+
+                    <div className="Description">
+                        <p id='desc-txt'>Kompetegram  merupakan komunitas untuk belajar dan berbagi pengetahuan mengenai teknologi. 
+                            Tidak hanya itu, di Kompetegram juga terdapat event atau kegiatan seperti webinar, projek, 
+                            dan kuis.
+                        </p>
+                    </div>
+
+                    <div className="Cards">
+                        <div className="Card-1">
+                            <p id='number'>54</p>
+                            <p id='text'>Anggota</p>
+                        </div>
+
+                        <div className="Card-2">
+                            <p id='number'>21</p>
+                            <p id='text'>Kegiatan</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         );
     }
 }
- 
+
 export default Home;
