c849c859d06a02068fa36badda07967a9b9c1e2a
Oleksandr F <boltbright525@gmail.com>
2022-10-07 19:08:34 +0700

    feat: added register path for react route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Opreg from "./pages/registration/Main"
 
 class App extends Component {
 
@@ -25,6 +26,7 @@ class App extends Component {
           <Routes>
             <Route exact path='/' element={ <Navigate to='/home' /> } />
             <Route exact path='/home' element={ <Home /> } />
+            <Route exact path='/registration' element={ <Opreg /> } />
           </Routes>
         </div>
       </BrowserRouter>
